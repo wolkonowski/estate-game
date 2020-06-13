87403d3da11d4f19c83e6dbb2e7cbd2d234adba3
Krzysztof <k.wolkonowski.solutions@gmail.com>
2020-06-14 01:33:08 +0200

    Updates

---
+++ precache-manifest.57ab3052c78186935057417950babf51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f65180e25c94daff7bf6485eecc9c80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e8ba45018e6133071a3",
+    "url": "/static/css/main.4dc80851.chunk.css"
+  },
+  {
+    "revision": "78f26efa7eb4d5535ec2",
+    "url": "/static/js/2.9d1f2ca9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d1f2ca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e8ba45018e6133071a3",
+    "url": "/static/js/main.a8037e10.chunk.js"
+  },
+  {
+    "revision": "37e354426cea94ade086",
+    "url": "/static/js/runtime-main.309b9b9c.js"
+  }
+]);
